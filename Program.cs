60c467a4444fe56032a55d5526fe76afdb56d52d
Soulkepeer123 <risterio@yandex.ru>
2018-02-22 18:13:38 +0700

    Eto rabotaet

--- Program.cs
+++ Program.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace WindowsFormsApplication1
 {
     public class Figure
     {
         public int x, y, c;
-        public Figure (int x, int y, int c)
+        public void setXYC (int x, int y, int c)
         {
             this.x = x;
             this.y = y;
             this.c = c;
+        }
+        public virtual void DoDraw( PaintEventArgs e)
+        {
+            // Pen pen = new Pen(Color.Red);
+            // e.Graphics.DrawEllipse(pen, x,y,c,c );
+        }
+        private void Form1_Paint(Object sender, PaintEventArgs e)
+        {
+            
         }
     }
     static class Program
--- WindowsFormsApplication2/WindowsFormsApplication2/Properties/AssemblyInfo.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("WindowsFormsApplication2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("WindowsFormsApplication2")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5674a0e2-4ca7-4c0a-b257-d12d505ea5ee")]
+[assembly: Guid("c1ca5014-51e9-4cc9-a1b9-8565aa00db04")]
 
 // Version information for an assembly consists of the following four values:
 //
